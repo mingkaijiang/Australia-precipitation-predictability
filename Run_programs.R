5f83d7fe74d7e7ff3a50531c2ba931ab42c3bb44
mingkaijiang <shanyujordan512@gmail.com>
2018-04-11 16:16:59 +1000

    first commit - setting up

---
+++ Run_programs.R
@@ -0,0 +1,26 @@
+######  This is a repo to create Australia precipitation predictability
+######  based on Colwell index and AWAP data (Australia Water Availability Project)
+######  Key refs:
+######  
+######  Data source: http://www.csiro.au/awap/
+######  Data readme: http://www.csiro.au/awap/doc/AWAP_readme_v8.txt
+######  Author: Mingkai Jiang (m.jiang@westernsydney.edu.au)
+###################################################################################
+
+###### 1. Download the data 
+#### Actually need to request it from CSIRO, so no automatic download
+
+###### 2. Process the data into the right format
+
+
+
+
+###### 3. Calculate predictability for each grid
+
+
+
+###### 4. Make some plots
+
+
+
+###### End.
