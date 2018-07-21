f4f1716b20410ea0657163c0855ed990ce6b3bfc
mingkaijiang <shanyujordan512@gmail.com>
2018-07-21 14:36:13 +1000

    added decile script

--- code/Calculate_predictability_decile.R
+++ code/Calculate_predictability_decile.R
@@ -36,168 +36,171 @@ Calculate_predictability_decile<- function(sourceDir, destDir) {
     yeare <- max(ts)
     yearr <- yeare-years
     
-    ### Define decile for each site
-    max_top <- 1
-    min_bot <- 0
+    ### Create matrix to store the frequency table, without defining bin sizes
     interval <- 10
-    diff <- max_top - min_bot
     bin <- matrix(0, ncol=14, nrow=interval)
     dimnames(bin) <- list(NULL,c("bin_size","Jan", "Feb", "Mar", "Apr", "May",
                                  "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "whole"))
-    bin[,"bin_size"] <- seq(0.1,1,0.1)
-    breaks = seq(0,1,0.1)
     
     ### output in each grid
     for (i in 1:nrow(out)) {
 
             ### Fill the temp DF, read for calculating predictability
-            ### the " + 0.0000001" adds values to all 0's to avoid NA
-            tmpDF[tmpDF$Year == 1980, 2:13] <- DF1[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1981, 2:13] <- DF2[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1982, 2:13] <- DF3[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1983, 2:13] <- DF4[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1984, 2:13] <- DF5[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1985, 2:13] <- DF6[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1986, 2:13] <- DF7[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1987, 2:13] <- DF8[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1988, 2:13] <- DF9[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1989, 2:13] <- DF10[i,3:14] + 0.0000001
+            tmpDF[tmpDF$Year == 1980, 2:13] <- DF1[i,3:14]
+            tmpDF[tmpDF$Year == 1981, 2:13] <- DF2[i,3:14]
+            tmpDF[tmpDF$Year == 1982, 2:13] <- DF3[i,3:14]
+            tmpDF[tmpDF$Year == 1983, 2:13] <- DF4[i,3:14]
+            tmpDF[tmpDF$Year == 1984, 2:13] <- DF5[i,3:14]
+            tmpDF[tmpDF$Year == 1985, 2:13] <- DF6[i,3:14]
+            tmpDF[tmpDF$Year == 1986, 2:13] <- DF7[i,3:14]
+            tmpDF[tmpDF$Year == 1987, 2:13] <- DF8[i,3:14]
+            tmpDF[tmpDF$Year == 1988, 2:13] <- DF9[i,3:14]
+            tmpDF[tmpDF$Year == 1989, 2:13] <- DF10[i,3:14]
             
-            tmpDF[tmpDF$Year == 1990, 2:13] <- DF11[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1991, 2:13] <- DF12[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1992, 2:13] <- DF13[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1993, 2:13] <- DF14[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1994, 2:13] <- DF15[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1995, 2:13] <- DF16[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1996, 2:13] <- DF17[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1997, 2:13] <- DF18[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1998, 2:13] <- DF19[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 1999, 2:13] <- DF20[i,3:14] + 0.0000001
+            tmpDF[tmpDF$Year == 1990, 2:13] <- DF11[i,3:14]
+            tmpDF[tmpDF$Year == 1991, 2:13] <- DF12[i,3:14]
+            tmpDF[tmpDF$Year == 1992, 2:13] <- DF13[i,3:14]
+            tmpDF[tmpDF$Year == 1993, 2:13] <- DF14[i,3:14]
+            tmpDF[tmpDF$Year == 1994, 2:13] <- DF15[i,3:14]
+            tmpDF[tmpDF$Year == 1995, 2:13] <- DF16[i,3:14]
+            tmpDF[tmpDF$Year == 1996, 2:13] <- DF17[i,3:14]
+            tmpDF[tmpDF$Year == 1997, 2:13] <- DF18[i,3:14]
+            tmpDF[tmpDF$Year == 1998, 2:13] <- DF19[i,3:14]
+            tmpDF[tmpDF$Year == 1999, 2:13] <- DF20[i,3:14]
             
-            tmpDF[tmpDF$Year == 2000, 2:13] <- DF21[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2001, 2:13] <- DF22[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2002, 2:13] <- DF23[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2003, 2:13] <- DF24[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2004, 2:13] <- DF25[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2005, 2:13] <- DF26[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2006, 2:13] <- DF27[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2007, 2:13] <- DF28[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2008, 2:13] <- DF29[i,3:14] + 0.0000001
-            tmpDF[tmpDF$Year == 2009, 2:13] <- DF30[i,3:14] + 0.0000001
+            tmpDF[tmpDF$Year == 2000, 2:13] <- DF21[i,3:14]
+            tmpDF[tmpDF$Year == 2001, 2:13] <- DF22[i,3:14]
+            tmpDF[tmpDF$Year == 2002, 2:13] <- DF23[i,3:14]
+            tmpDF[tmpDF$Year == 2003, 2:13] <- DF24[i,3:14]
+            tmpDF[tmpDF$Year == 2004, 2:13] <- DF25[i,3:14]
+            tmpDF[tmpDF$Year == 2005, 2:13] <- DF26[i,3:14]
+            tmpDF[tmpDF$Year == 2006, 2:13] <- DF27[i,3:14]
+            tmpDF[tmpDF$Year == 2007, 2:13] <- DF28[i,3:14]
+            tmpDF[tmpDF$Year == 2008, 2:13] <- DF29[i,3:14]
+            tmpDF[tmpDF$Year == 2009, 2:13] <- DF30[i,3:14]
 
-            ### calculate as percentage of annual total
-            tmpDF2 <- tmpDF[,1:13]
-            ann.tot <- rowSums(tmpDF[,2:13])
+            ### Now can define the deciles
+            decile.list <- quantile(data.matrix(tmpDF[,2:13]), 
+                                    prob=seq(0,1, length=11))
             
-            for (k in 2:13) {
-                tmpDF2[,k] <- tmpDF[,k] / ann.tot
-            } 
+            max_top <- decile.list[11]
+            min_bot <- decile.list[0]
+            diff <- max_top - min_bot
             
-            ### Cut the tables
-            jan_cut = cut(tmpDF2[, "Jan"], breaks, include.lowest=TRUE,right=TRUE)
-            feb_cut = cut(tmpDF2[, "Feb"], breaks, include.lowest=TRUE,right=TRUE)
-            mar_cut = cut(tmpDF2[, "Mar"], breaks, include.lowest=TRUE,right=TRUE)
-            apr_cut = cut(tmpDF2[, "Apr"], breaks, include.lowest=TRUE,right=TRUE)
-            may_cut = cut(tmpDF2[, "May"], breaks, include.lowest=TRUE,right=TRUE)
-            jun_cut = cut(tmpDF2[, "Jun"], breaks, include.lowest=TRUE,right=TRUE)
-            jul_cut = cut(tmpDF2[, "Jul"], breaks, include.lowest=TRUE,right=TRUE)
-            aug_cut = cut(tmpDF2[, "Aug"], breaks, include.lowest=TRUE,right=TRUE)
-            sep_cut = cut(tmpDF2[, "Sep"], breaks, include.lowest=TRUE,right=TRUE)
-            oct_cut = cut(tmpDF2[, "Oct"], breaks, include.lowest=TRUE,right=TRUE)
-            nov_cut = cut(tmpDF2[, "Nov"], breaks, include.lowest=TRUE,right=TRUE)
-            dec_cut = cut(tmpDF2[, "Dec"], breaks, include.lowest=TRUE,right=TRUE)
+            bin[,"bin_size"] <- decile.list[2:11]
+            breaks = decile.list
             
-            ### Frequency tables
-            jan_freq = table(jan_cut)
-            feb_freq = table(feb_cut)
-            mar_freq = table(mar_cut)
-            apr_freq = table(apr_cut)
-            may_freq = table(may_cut)
-            jun_freq = table(jun_cut)
-            jul_freq = table(jul_cut)
-            aug_freq = table(aug_cut)
-            sep_freq = table(sep_cut)
-            oct_freq = table(oct_cut)
-            nov_freq = table(nov_cut)
-            dec_freq = table(dec_cut)
-            
-            ### Assign onto bins
-            bin[,"Jan"] <- jan_freq
-            bin[,"Feb"] <- feb_freq
-            bin[,"Mar"] <- mar_freq
-            bin[,"Apr"] <- apr_freq
-            bin[,"May"] <- may_freq
-            bin[,"Jun"] <- jun_freq
-            bin[,"Jul"] <- jul_freq
-            bin[,"Aug"] <- aug_freq
-            bin[,"Sep"] <- sep_freq
-            bin[,"Oct"] <- oct_freq
-            bin[,"Nov"] <- nov_freq
-            bin[,"Dec"] <- dec_freq
-            
-            bin[,"whole"] = rowSums(bin[,2:13])
-            
-            col_sum <- length(ts)   # total number count of years, i.e. 30
-            whole_sum <- col_sum*12
-            
-            #uncertainty with respect to time H(X)
-            HofX <- -((col_sum/whole_sum)*log10(col_sum/whole_sum))*12
-            
-            #uncertainty with respect to state H(Y)
-            V1 <- bin[,"whole"]/whole_sum
-            V2 <- log10(bin[,"whole"]/whole_sum)
-            for (k in 1:length(V2)) {
-                if(is.finite(V2[k])==F) {
-                    V2[k] <- 0 
-                } else {
-                    V2[k] <- V2[k]
+            ### check: if repeated decile, ignore this site
+            if (decile.list[1]==decile.list[2]) {
+                out[i,"P"] <- NA
+                out[i,"C"] <- NA
+                out[i,"M"] <- NA
+            } else {
+                ### Cut the tables
+                jan_cut = cut(tmpDF[, "Jan"], breaks, include.lowest=TRUE,right=TRUE)
+                feb_cut = cut(tmpDF[, "Feb"], breaks, include.lowest=TRUE,right=TRUE)
+                mar_cut = cut(tmpDF[, "Mar"], breaks, include.lowest=TRUE,right=TRUE)
+                apr_cut = cut(tmpDF[, "Apr"], breaks, include.lowest=TRUE,right=TRUE)
+                may_cut = cut(tmpDF[, "May"], breaks, include.lowest=TRUE,right=TRUE)
+                jun_cut = cut(tmpDF[, "Jun"], breaks, include.lowest=TRUE,right=TRUE)
+                jul_cut = cut(tmpDF[, "Jul"], breaks, include.lowest=TRUE,right=TRUE)
+                aug_cut = cut(tmpDF[, "Aug"], breaks, include.lowest=TRUE,right=TRUE)
+                sep_cut = cut(tmpDF[, "Sep"], breaks, include.lowest=TRUE,right=TRUE)
+                oct_cut = cut(tmpDF[, "Oct"], breaks, include.lowest=TRUE,right=TRUE)
+                nov_cut = cut(tmpDF[, "Nov"], breaks, include.lowest=TRUE,right=TRUE)
+                dec_cut = cut(tmpDF[, "Dec"], breaks, include.lowest=TRUE,right=TRUE)
+                
+                ### Frequency tables
+                jan_freq = table(jan_cut)
+                feb_freq = table(feb_cut)
+                mar_freq = table(mar_cut)
+                apr_freq = table(apr_cut)
+                may_freq = table(may_cut)
+                jun_freq = table(jun_cut)
+                jul_freq = table(jul_cut)
+                aug_freq = table(aug_cut)
+                sep_freq = table(sep_cut)
+                oct_freq = table(oct_cut)
+                nov_freq = table(nov_cut)
+                dec_freq = table(dec_cut)
+                
+                ### Assign onto bins
+                bin[,"Jan"] <- jan_freq
+                bin[,"Feb"] <- feb_freq
+                bin[,"Mar"] <- mar_freq
+                bin[,"Apr"] <- apr_freq
+                bin[,"May"] <- may_freq
+                bin[,"Jun"] <- jun_freq
+                bin[,"Jul"] <- jul_freq
+                bin[,"Aug"] <- aug_freq
+                bin[,"Sep"] <- sep_freq
+                bin[,"Oct"] <- oct_freq
+                bin[,"Nov"] <- nov_freq
+                bin[,"Dec"] <- dec_freq
+                
+                bin[,"whole"] = rowSums(bin[,2:13])
+                
+                col_sum <- length(ts)   # total number count of years, i.e. 30
+                whole_sum <- col_sum*12
+                
+                #uncertainty with respect to time H(X)
+                HofX <- -((col_sum/whole_sum)*log10(col_sum/whole_sum))*12
+                
+                #uncertainty with respect to state H(Y)
+                V1 <- bin[,"whole"]/whole_sum
+                V2 <- log10(bin[,"whole"]/whole_sum)
+                for (k in 1:length(V2)) {
+                    if(is.finite(V2[k])==F) {
+                        V2[k] <- 0 
+                    } else {
+                        V2[k] <- V2[k]
+                    }
                 }
-            }
-            
-            HofY <- -sum(V1*V2)
-            
-            #uncertainty with respect to interaction of time and state, H(XY)
-            M1 <- bin[1:interval,2:13]/whole_sum
-            M2 <- log10(M1)
-            for (k in 1:length(M2)) {
-                if(is.finite(M2[k])==F) {
-                    M2[k] <- 0
-                } else {
-                    M2[k] <- M2[k]
+                
+                HofY <- -sum(V1*V2)
+                
+                #uncertainty with respect to interaction of time and state, H(XY)
+                M1 <- bin[1:interval,2:13]/whole_sum
+                M2 <- log10(M1)
+                for (k in 1:length(M2)) {
+                    if(is.finite(M2[k])==F) {
+                        M2[k] <- 0
+                    } else {
+                        M2[k] <- M2[k]
+                    }
                 }
+                
+                HofXY <- -sum(M1*M2)
+                
+                #Conditional uncertainty with regard to state, with time given, HXofY
+                HXofY <- HofXY - HofX
+                s <- interval
+                t <- 12
+                
+                #predictability (P), constancy(C) and contingency (M)
+                P <- 1-(HXofY/log10(s))
+                C <- 1-(HofY/log10(s))
+                M <- (HofX+HofY-HofXY)/log10(s)
+                
+                #mutual information, I(XY)
+                IofXY <- HofY - HXofY
+                
+                #deviation from homogeneity of the columns of the matrix for constancy, GC
+                GC <- 2*whole_sum*(log(s)-HofY)
+                C_free <- s-1
+                
+                #deviation from homogeneity of the columns of the matrix for contingency, GM
+                GM <- 2*whole_sum*(HofX+HofY-HofXY)
+                M_free <- (s-1)*(t-1)
+                
+                #deviation from homogeneity of the columns of the matrix for predictability, GP
+                GP <- GM + GC
+                P_free <- (s-1)*t
+                
+                out[i,"P"] <- P
+                out[i,"C"] <- C
+                out[i,"M"] <- M
             }
-            
-            HofXY <- -sum(M1*M2)
-            
-            #Conditional uncertainty with regard to state, with time given, HXofY
-            HXofY <- HofXY - HofX
-            s <- interval
-            t <- 12
-            
-            #predictability (P), constancy(C) and contingency (M)
-            P <- 1-(HXofY/log10(s))
-            C <- 1-(HofY/log10(s))
-            M <- (HofX+HofY-HofXY)/log10(s)
-            
-            #mutual information, I(XY)
-            IofXY <- HofY - HXofY
-            
-            #deviation from homogeneity of the columns of the matrix for constancy, GC
-            GC <- 2*whole_sum*(log(s)-HofY)
-            C_free <- s-1
-            
-            #deviation from homogeneity of the columns of the matrix for contingency, GM
-            GM <- 2*whole_sum*(HofX+HofY-HofXY)
-            M_free <- (s-1)*(t-1)
-            
-            #deviation from homogeneity of the columns of the matrix for predictability, GP
-            GP <- GM + GC
-            P_free <- (s-1)*t
-            
-            out[i,"P"] <- P
-            out[i,"C"] <- C
-            out[i,"M"] <- M
-
     }   
     
     write.csv(out, paste0(destDir, "/Australia_rainfall_predictability_0.5_resolution_2.csv"),
